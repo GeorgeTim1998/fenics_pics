fc0242665a685726345785d2665a4154b6590edd
georgetim1998 <georgetim1998@gmail.com>
2022-06-14 20:34:32 +0300

    update concat files from folders

--- combine_folder.py
+++ combine_folder.py
@@ -3,9 +3,9 @@ import os
 from PIL import Image
 import numpy as np
 
-folder1 = "Novessel#2"
-folder2 = "Vessel#2"
-concat_folder= "Concat#2"
+folder1 = "Pics/Novessel#3"
+folder2 = "Pics/AbsoluteVessel#1"
+concat_folder= "Pics/Concat#3"
 
 # for i in sorted(os.listdir(folder1)):
 #     print(i)
@@ -18,6 +18,10 @@ files2 = sorted(os.listdir(folder2))
 # print(len(os.listdir('Novessel#2')))
 
 for i in range(len(files1)):
+    print("%d/%d -> %.2f" % (i+1, 
+                             len(files1),
+                             (i+1)/len(files1)*100))
+    
     im1 = Image.open(os.path.join(folder1, files1[i]))
     im2 = Image.open(os.path.join(folder2, files2[i]))
     
