ca02e4eedede74fd48513590c19a03c85f43a7df
georgetim1998 <georgetim1998@gmail.com>
2022-08-15 15:19:52 +0300

    \n filter

--- delete_carriage.py
+++ delete_carriage.py
@@ -2,7 +2,8 @@ with open('Texts/pre_carriage.txt') as f:
     lines = f.readlines()
 
 for i in range(len(lines)):
-    lines[i] = lines[i].replace("\n", " ")
+    if lines[i] != "\n":
+        lines[i] = lines[i].replace("\n", " ")
 
 
 lines = ' '.join(lines)
