9c5fd5c193cc6c4a311c52bb798096278eb4b0c7
georgetim1998 <georgetim1998@gmail.com>
2022-08-23 23:24:59 +0300

    marinas comments

--- delete_carriage.py
+++ delete_carriage.py
@@ -2,6 +2,5 @@ from tkinter import Tk
 
 clipboard_text = Tk().clipboard_get().replace("\n", " ")
 
-file = open("Texts/post_carriage.txt", "a")
-file.write(clipboard_text + "\n\n")
-file.close()
+with open("Texts/post_carriage.txt", "a") as file: 
+    file.write(clipboard_text + "\n\n")
