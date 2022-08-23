6d3324bcfa515bd8b4841b5c0c1d35aee3e15e05
georgetim1998 <georgetim1998@gmail.com>
2022-08-23 21:00:26 +0300

    \n added for better text/notion interaction

--- delete_carriage.py
+++ delete_carriage.py
@@ -2,6 +2,6 @@ from tkinter import Tk
 
 clipboard_text = Tk().clipboard_get().replace("\n", " ")
 
-file = open("Texts/post_carriage.txt", "w")
-file.write(clipboard_text)
+file = open("Texts/post_carriage.txt", "a")
+file.write(clipboard_text + "\n\n")
 file.close()
