236117976b3ae014f476db32aff4d7b933363871
georgetim1998 <georgetim1998@gmail.com>
2022-09-08 18:54:37 +0300

    added point

--- delete_carriage.py
+++ delete_carriage.py
@@ -3,7 +3,7 @@ from tkinter import Tk
 
 clipboard_text = Tk().clipboard_get()
 clipboard_text = re.sub(r"\n([^А-Я|^A-Z])", r" \1", clipboard_text)
-clipboard_text = re.sub(r"(\w)\n(\w)", r"\1\n\n\2", clipboard_text)
+clipboard_text = re.sub(r"(\w.)\n(\w)", r"\1\n\n\2", clipboard_text)
 clipboard_text = re.sub(r"(\w)( -|- )(\w)", r"\1 - \3", clipboard_text)
 
 with open("Texts/post_carriage.txt", "a") as file: 
