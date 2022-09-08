61389f805516c4c2ea3c7abf4894a79ac807d099
georgetim1998 <georgetim1998@gmail.com>
2022-09-08 22:31:52 +0300

    rename

--- delete_carriage.py
+++ delete_carriage.py
@@ -1,7 +1,7 @@
 import re
 from tkinter import Tk
 
-patterns = {
+re_sub_args = {
     r"\n([^А-Я|^A-Z])": r" \1",
     r"(\w)\n([А-Я|A-Z])": r"\1 \2",
     r"(\w.)\n(\w)": r"\1\n\n\2",
@@ -10,7 +10,7 @@ patterns = {
 
 clipboard_text = Tk().clipboard_get()
 
-for pattern, repl in patterns.items():
+for pattern, repl in re_sub_args.items():
     clipboard_text = re.sub(pattern, repl, clipboard_text)
 
 with open("Texts/post_carriage.txt", "a") as file: 
