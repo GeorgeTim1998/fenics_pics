b31ea4a3ac00a83d99af872d019dfe2c169f07ce
georgetim1998 <georgetim1998@gmail.com>
2022-09-08 22:30:42 +0300

    Щзешьшяу

--- delete_carriage.py
+++ delete_carriage.py
@@ -1,11 +1,17 @@
 import re
 from tkinter import Tk
 
+patterns = {
+    r"\n([^А-Я|^A-Z])": r" \1",
+    r"(\w)\n([А-Я|A-Z])": r"\1 \2",
+    r"(\w.)\n(\w)": r"\1\n\n\2",
+    r"(\w)( -|- )(\w)": r"\1 - \3"
+    }
+
 clipboard_text = Tk().clipboard_get()
-clipboard_text = re.sub(r"\n([^А-Я|^A-Z])", r" \1", clipboard_text)
-clipboard_text = re.sub(r"(\w)\n([А-Я|A-Z])", r"\1 \2", clipboard_text)
-clipboard_text = re.sub(r"(\w.)\n(\w)", r"\1\n\n\2", clipboard_text)
-clipboard_text = re.sub(r"(\w)( -|- )(\w)", r"\1 - \3", clipboard_text)
+
+for pattern, repl in patterns.items():
+    clipboard_text = re.sub(pattern, repl, clipboard_text)
 
 with open("Texts/post_carriage.txt", "a") as file: 
     file.write("\n" + clipboard_text + "\n")
