dc78c6356c24a77b109bceeca06996bce40c2e6c
georgetim1998 <georgetim1998@gmail.com>
2022-09-08 16:53:16 +0300

    with regex

--- delete_carriage.py
+++ delete_carriage.py
@@ -1,6 +1,9 @@
+import re
 from tkinter import Tk
 
-clipboard_text = Tk().clipboard_get().replace("\n", " ")
+clipboard_text = Tk().clipboard_get()
+clipboard_text = re.sub(r"\n([^А-Я])", r" \1", clipboard_text)
+clipboard_text = clipboard_text.replace("\n", "\n\n")
 
 with open("Texts/post_carriage.txt", "a") as file: 
     file.write(clipboard_text + "\n\n")
